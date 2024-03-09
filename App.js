f14dfce5fe6eecf31e151bcbf95e8077df207809
thales0vinicius <thales.vinicius.silveira@gmail.com>
2024-03-09 13:50:38 -0300

    <3

--- App.js
+++ App.js
@@ -1,61 +1,66 @@
-import { StyleSheet, Text, View, TouchableOpacity, Image, } from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  StatusBar,
+} from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <StatusBar></StatusBar>
+
       <View style={styles.logo}>
-        <Image
-          source={require('./assets/Relógio.png')} style={styles.img1}
-        />
+        <Image source={require("./assets/Relógio.png")} style={styles.img1} />
+
         <Text style={styles.texto1}>ListOne</Text>
       </View>
-      <Image
-        source={require('./assets/desenho.png')} style={styles.img2}
-      />
-      <Text style={styles.texto2}>
-        Organize seu trabalho e sua
-      </Text >
-      <Text style={styles.texto3}>
-        vida de uma forma simples.
-      </Text>
+
+      <Image source={require("./assets/Desenho.png")} style={styles.img2} />
+
+      <Text style={styles.texto2}>Organize seu trabalho e sua</Text>
+
+      <Text style={styles.texto3}>vida de uma forma simples.</Text>
+
       <TouchableOpacity style={styles.button}>
-        <Image
-          source={require('./assets/Google.png')} style={styles.img3}
-        />
-        <Text style={styles.texto4}>
-          Entrar com Google
-        </Text>
+        <Image source={require("./assets/Google.png")} style={styles.img3} />
+
+        <Text style={styles.texto4}>Entrar com Google</Text>
       </TouchableOpacity>
+
       <TouchableOpacity style={styles.button}>
-        <Image
-          source={require('./assets/Email.png')} style={styles.img4}
-        />
-        <Text style={styles.texto4}>
-          Entrar com E-mail
-        </Text>
+        <Image source={require("./assets/Email.png")} style={styles.img4} />
+
+        <Text style={styles.texto5}>Entrar com E-mail</Text>
       </TouchableOpacity>
-      <Text style={styles.texto5}>
-        Não tenho uma conta...
-      </Text>
-      <Text style={styles.texto6}>
-        Ao continuar com os serviços acima, você concorda com os "Termos de Serviço" e com a "Política de Privacidade"
+
+      <TouchableOpacity>
+        <Text style={styles.texto6}>Não tenho uma conta...</Text>
+      </TouchableOpacity>
+
+      <Text style={styles.texto7}>
+        Ao continuar com os serviços acima, você concorda com os "Termos de
+        Serviço" e com a "Política de Privacidade"
       </Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+
   container: {
+    backgroundColor: "#140D0D",
     flex: 1,
     gap: 10,
-    backgroundColor: '#140D0D',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
 
   input: {
     backgroundColor: "#fff",
-    width: '50%',
+    width: "50%",
     borderRadius: 8,
     borderWidth: 1,
     paddingHorizontal: 8,
@@ -63,9 +68,9 @@ const styles = StyleSheet.create({
   },
 
   button: {
-    flexDirection: 'row',
     backgroundColor: "#140D0D",
-    borderColor: '#fff',
+    borderColor: "#fff",
+    flexDirection: "row",
     borderRadius: 8,
     borderWidth: 1,
     paddingHorizontal: 24,
@@ -73,72 +78,82 @@ const styles = StyleSheet.create({
   },
 
   logo: {
-    flexDirection: 'row',
+    flexDirection: "row",
   },
 
   img1: {
-    marginTop: 7,
-    marginRight: 10,
     width: 25,
-    height: 24,
+    height: 25,
+    marginTop: 12,
+    marginRight: 7,
   },
 
   img2: {
-    width: 250,
-    height: 240,
+    width: 230,
+    height: 230,
   },
 
   img3: {
     width: 20,
     height: 20,
+    marginLeft: -1,
   },
 
   img4: {
     width: 27,
     height: 25,
-
+    marginRight: 4,
+    marginLeft: -3,
   },
 
   texto1: {
     color: "#fff",
-    fontSize: 24
+    fontSize: 30,
   },
 
   texto2: {
-    textAlign: 'center',
+    textAlign: "center",
     color: "#fff",
-    fontSize: 14,
-    marginTop: 10,
+    fontSize: 20,
+    marginTop: 3,
   },
 
   texto3: {
     color: "#fff",
-    textAlign: 'center',
-    fontSize: 14,
+    textAlign: "center",
+    fontSize: 18,
     marginTop: -10,
-    marginBottom: 10,
+    marginBottom: 15,
   },
 
   texto4: {
     color: "#fff",
-    textAlign: 'center',
-    marginLeft: 10,
+    textAlign: "center",
     fontSize: 14,
+    marginLeft: 10,
   },
 
   texto5: {
     color: "#fff",
-    textAlign: 'center',
-    textDecorationLine: 'underline',
-    fontSize: 12,
-    marginTop: 2,
-    gap: 12,
+    textAlign: "center",
+    fontSize: 14,
+    marginLeft: 4,
+    marginRight: 5,
   },
 
   texto6: {
     color: "#fff",
-    textAlign: 'center',
+    textAlign: "center",
+    textDecorationLine: "underline",
+    fontSize: 12,
+    gap: 12,
+    marginTop: 2,
+  },
+
+  texto7: {
+    color: "#fff",
+    textAlign: "center",
     fontSize: 10,
-    marginTop: 10,
+    marginTop: 30,
   },
 });

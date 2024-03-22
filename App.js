613f364b27cb7b1b191aa19ddc19ef19a3e31124
thales0vinicius <thales.vinicius.silveira@gmail.com>
2024-03-22 14:33:48 -0300

    <3

--- App.js
+++ App.js
@@ -1,49 +1,31 @@
-import {
-  StyleSheet,
-  View,
-  Text,
-  Image,
-  TouchableOpacity,
-  StatusBar,
-} from "react-native";
+import {StyleSheet, View, Text, Image, TouchableOpacity, StatusBar, } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar></StatusBar>
-      
       <View style={styles.logo}>
         <Image source={require("./assets/relógio.png")} style={styles.img1} />
-
         <Text style={styles.texto1}>ListOne</Text>
       </View>
-
       <Image source={require("./assets/desenho.png")} style={styles.img2} />
-
       <Text style={styles.texto2}>Organize seu trabalho</Text>
-
       <Text style={styles.texto3}>e vida de uma forma</Text>
-
       <Text style={styles.texto4}>simples.</Text>
-
       <TouchableOpacity style={styles.button}>
-      <Image source={require("./assets/google.png")} style={styles.img3}/>
-
-      <Text style={styles.texto5}>Entrar com Google</Text>
+        <Image source={require("./assets/google.png")} style={styles.img3} />
+        <Text style={styles.texto5}>Entrar com Google</Text>
       </TouchableOpacity>
-
       <TouchableOpacity style={styles.button}>
-      <Image source={require("./assets/email.png")} style={styles.img3}/>
-
-      <Text style={styles.texto5}>Entrar com E-mail</Text>
+        <Image source={require("./assets/email.png")} style={styles.img3} />
+        <Text style={styles.texto5}>Entrar com E-mail</Text>
       </TouchableOpacity>
-
       <TouchableOpacity>
         <Text style={styles.texto6}>Não tenho uma conta...</Text>
       </TouchableOpacity>
-
       <Text style={styles.texto7}>
-        Ao continuar com os serviços acima, você concorda com os: "Termos de Serviço" e com a "Política de Privacidade"
+        Ao continuar com os serviços acima, você concorda com os: "Termos de
+        Serviço" e com a "Política de Privacidade"
       </Text>
     </View>
   );
@@ -56,13 +38,12 @@ const styles = StyleSheet.create({
     gap: 10,
     alignItems: "center",
     justifyContent: "center",
-    
   },
 
   button: {
     backgroundColor: "#140D0D",
     borderColor: "#fff",
-    borderRadius: 80,
+    borderRadius: 10,
     borderWidth: 1,
     paddingVertical: 8,
     flexDirection: "row",
@@ -105,7 +86,7 @@ const styles = StyleSheet.create({
   texto3: {
     color: "#fff",
     fontSize: 18,
-    marginTop: -10, 
+    marginTop: -10,
   },
 
   texto4: {
@@ -125,13 +106,12 @@ const styles = StyleSheet.create({
     color: "#fff",
     textDecorationLine: "underline",
     fontSize: 14,
-    marginTop: 6,
   },
 
   texto7: {
     color: "#fff",
     textAlign: "center",
     fontSize: 10,
-    marginTop: 30,
+    marginTop: 40,
   },
 });
